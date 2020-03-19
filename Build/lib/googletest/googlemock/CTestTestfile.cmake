0aba1a37ca4eb153a31e694139989090a4136f66
rexx.magtar <rexx.magtar@gmail.com>
2020-03-20 02:24:08 +0300

    testing

--- Build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Lab5TPNo-money/lib/googletest/googlemock
-# Build directory: C:/Lab5TPNo-money/Build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
