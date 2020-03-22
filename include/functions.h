7acdbe9edde43c7d0b748857460e6a4af85bb2a1
maksimlihoi <lihoi.maksim@gmail.com>
2020-03-22 23:27:03 +0300

    Added functions.h

---
+++ include/functions.h
@@ -0,0 +1,4 @@
+#pragma once
+
+char*** CrewMemberInformation(char* surname, char*** columnName, int* rowCount, int* columnCount);
+char*** GetFlightInformation(int special, char*** columnName, int* rowCount, int* columnCount);
