495a893667c5877690c68fe7d11dbc44da17c313
rexx.magtar <rexx.magtar@gmail.com>
2020-03-23 01:02:43 +0300

    cmake edited

--- src/FrontLogic/Authorization.c
+++ src/FrontLogic/Authorization.c
@@ -1,7 +1,11 @@
-#include "../include/Authorization.h"
+#include "../../include/Authorization.h"
+
+#include "../../BackEndLogic/sqlite3.h"
 #include "string.h"
 int ValidateUser(char* login, char* password)
 {
+	sqlite3* pedp;
+	sqlite3_open("dsd", &pedp);
 	if (strcmp(login, "admin") == 0 && strcmp(password, "1111")==0)
 	{
 		return  1;
--- src/FrontLogic/mainFront.c
+++ src/FrontLogic/mainFront.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../include/UIController.h"
+#include "../../include/UIController.h"
 
 int main() {
     UiControllerInit();
