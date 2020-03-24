d3922cfa50d62977e54bca61f10a7cf9100c7efc
angelina <angelinadrepakova001@gmail.com>
2020-03-24 15:10:12 +0300

    Test_CrewMemberInfomation

--- tst/Formula-test.cpp
+++ tst/Formula-test.cpp
@@ -1,26 +1,76 @@
 #include "gtest/gtest.h"
 extern  "C" {
-#include "../include/UIController.h"
+#include "../include/functions.h"
 }
 
 TEST(TryLogin, timesType) {
-    //arrange
-    //act
-    //assert
-    UiControllerInit();
-    EXPECT_EQ (TryLogin("user1","1234"),  -1);
-    EXPECT_EQ(TryLogin("admin", "1234"), -1);
-    EXPECT_EQ(TryLogin("admin", "12345"), -1);
-    EXPECT_EQ(TryLogin("admin", "1111"), -2);
+	char*** result = CrewMemberInfomation("Shestopalov");
+	EXPECT_EQ(result[0][0], "1");
+	EXPECT_EQ(result[0][1], "2020-02-01");
+	EXPECT_EQ(result[0][2], "1");
+	EXPECT_EQ(result[0][3], "12");
+	EXPECT_EQ(result[0][4], "5");
+	EXPECT_EQ(result[0][5], "2");
+	EXPECT_EQ(result[0][6], "5000");
+	EXPECT_EQ(result[0][7], "no");
+	EXPECT_EQ(result[1][0], "6");
+	EXPECT_EQ(result[1][1], "2020-02-07");
+	EXPECT_EQ(result[1][2], "1");
+	EXPECT_EQ(result[1][3], "18");
+	EXPECT_EQ(result[1][4], "15");
+	EXPECT_EQ(result[1][5], "4");
+	EXPECT_EQ(result[1][6], "7000");
+	EXPECT_EQ(result[1][7], "no");
 }
-
-TEST(TryLogin, correctLogin) {
-    //arrange
-    //act
-    //assert
-    UiControllerInit();
-    EXPECT_EQ(TryLogin("admin", "1234"), -1);
-    EXPECT_EQ(TryLogin("user1", "1111"), -1);
-    EXPECT_EQ(TryLogin("admin", "1111"), 1);
+TEST(TryLogin, timesType) {
+	char*** result = CrewMemberInfomation("Shestopalov");
+	EXPECT_NE(result[0][0], "4");
+	EXPECT_NE(result[0][1], "2020-06-07");
+	EXPECT_NE(result[0][2], "D");
+	EXPECT_NE(result[0][3], "18");
+	EXPECT_NE(result[0][4], "15");
+	EXPECT_NE(result[0][5], "6");
+	EXPECT_NE(result[0][6], " ");
+	EXPECT_NE(result[0][7], "yes");
+}
+TEST(TryLogin, timesType) {
+	char*** result = CrewMemberInfomation("Pirsky");
+	EXPECT_EQ(result[0][0], "2");
+	EXPECT_EQ(result[0][1], "2020-02-01");
+	EXPECT_EQ(result[0][2], "2");
+	EXPECT_EQ(result[0][3], "20");
+	EXPECT_EQ(result[0][4], "3");
+	EXPECT_EQ(result[0][5], "5");
+	EXPECT_EQ(result[0][6], "12000");
+	EXPECT_EQ(result[0][7], "yes");
+	EXPECT_EQ(result[1][0], "7");
+	EXPECT_EQ(result[1][1], "2020-02-08");
+	EXPECT_EQ(result[1][2], "2");
+	EXPECT_EQ(result[1][3], "17");
+	EXPECT_EQ(result[1][4], "20");
+	EXPECT_EQ(result[1][5], "10");
+	EXPECT_EQ(result[1][6], "14000");
+	EXPECT_EQ(result[1][7], "yes");
+	}
+TEST(TryLogin, timesType) {
+	char*** result = CrewMemberInfomation("Pirsky");
+	EXPECT_NE(result[0][0], "0");
+	EXPECT_NE(result[0][1], "2020-06-09");
+	EXPECT_NE(result[0][2], "F");
+	EXPECT_NE(result[0][3], "18");
+	EXPECT_NE(result[0][4], "15");
+	EXPECT_NE(result[0][5], "6");
+	EXPECT_NE(result[0][6], " ");
+	EXPECT_NE(result[0][7], "yes");
+	EXPECT_EQ(result[1][0], "15");
+	EXPECT_EQ(result[1][1], "2020-00-08");
+	EXPECT_EQ(result[1][2], "2");
+	EXPECT_EQ(result[1][3], "67");
+	EXPECT_EQ(result[1][4], "20");
+	EXPECT_EQ(result[1][5], "10");
+	EXPECT_EQ(result[1][6], "14000");
+	EXPECT_EQ(result[1][7], "no");
+}
+}
 }
 
