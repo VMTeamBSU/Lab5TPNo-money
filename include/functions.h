b2eb5f8361710b0442f649416b0d8b80424f40db
maksimlihoi <lihoi.maksim@gmail.com>
2020-03-24 11:34:11 +0300

    Initialization .h added

--- include/functions.h
+++ include/functions.h
@@ -2,3 +2,4 @@
 
 char*** CrewMemberInformation(char* surname, char*** columnName, int* rowCount, int* columnCount);
 char*** GetFlightInformation(int special, char*** columnName, int* rowCount, int* columnCount);
+char*** DateHelicopterInformation(char* date, char*** columnName, int* rowCount, int* columnCount);
