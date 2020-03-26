e1513847883dfe77b5599a6ee80a977cab712126
Ivan <51932532+rexxmagtar@users.noreply.github.com>
2020-03-26 18:40:25 +0300

    New functions added (#16)
    Co-authored-by: maksimlihoi <lihoi.maksim@gmail.com>

--- src/BackEndLogic/functions.c
+++ src/BackEndLogic/functions.c
@@ -63,13 +63,97 @@ char*** DateHelicopterInformation(char* firstDate, char* secondDate, char*** col
 
 	return Result;
 }
-//тест
+
 char*** HelicopterFlyDurationAndFlyingResourse(int helicopterId, char*** columnName, int* rowCount, int* columnCount)
 {
 	char requestBuffer[1000];
 	sprintf(requestBuffer,
 		"SELECT sum(duration), (flying_resourse - sum(duration)) FROM helicopter INNER JOIN flights ON flights.ID_helicopter = helicopter.ID WHERE ID_helicopter = %d; ",
 		helicopterId);
+	char** ColumnName = NULL;
+	int row = 0;
+	int column = 0;
+	char*** Result = GetResult(requestBuffer, &column, &row, &ColumnName);
+	*rowCount = row;
+	*columnCount = column;
+	*columnName = ColumnName;
+
+	return Result;
+}
+
+char*** MaxflughtsCrewInfo(char*** columnName, int* rowCount, int* columnCount)
+{
+	char requestBuffer[1000] = "SELECT crew.*, SUM(price) FROM crew, flights INNER JOIN commander On commander.id = crew.ID_commander WHERE commander.id_helicopter = (SELECT id_helicopter FROM flights GROUP By id_helicopter ORDER BY COUNT(id_helicopter) DESC LIMIT 1) GROUP BY crew.ID";
+	char** ColumnName = NULL;
+	int row = 0;
+	int column = 0;
+	char*** Result = GetResult(requestBuffer, &column, &row, &ColumnName);
+	*rowCount = row;
+	*columnCount = column;
+	*columnName = ColumnName;
+
+	return Result;
+}
+
+char*** MaxEarnedMoneyCrewInfo(char*** columnName, int* rowCount, int* columnCount)
+{
+	char requestBuffer[1000] = "SELECT crew.*, SUM(price) FROM crew, flights INNER JOIN commander On commander.id = crew.ID_commander WHERE commander.id_helicopter = (SELECT id_helicopter FROM flights GROUP By id_helicopter ORDER BY SUM(price) DESC LIMIT 1) GROUP BY crew.ID";
+	char** ColumnName = NULL;
+	int row = 0;
+	int column = 0;
+	char*** Result = GetResult(requestBuffer, &column, &row, &ColumnName);
+	*rowCount = row;
+	*columnCount = column;
+	*columnName = ColumnName;
+
+	return Result;
+}
+
+char*** ShowAllHelicopters(char*** columnName, int* rowCount, int* columnCount)
+{
+	char requestBuffer[1000] = "SELECT * FROM helicopter;";
+	char** ColumnName = NULL;
+	int row = 0;
+	int column = 0;
+	char*** Result = GetResult(requestBuffer, &column, &row, &ColumnName);
+	*rowCount = row;
+	*columnCount = column;
+	*columnName = ColumnName;
+
+	return Result;
+}
+
+char*** ShowAllCrewMembers(char*** columnName, int* rowCount, int* columnCount)
+{
+	char requestBuffer[1000] = "SELECT * FROM crew;";
+	char** ColumnName = NULL;
+	int row = 0;
+	int column = 0;
+	char*** Result = GetResult(requestBuffer, &column, &row, &ColumnName);
+	*rowCount = row;
+	*columnCount = column;
+	*columnName = ColumnName;
+
+	return Result;
+}
+
+char*** ShowAllFlights(char*** columnName, int* rowCount, int* columnCount)
+{
+	char requestBuffer[1000] = "SELECT * FROM flights;";
+	char** ColumnName = NULL;
+	int row = 0;
+	int column = 0;
+	char*** Result = GetResult(requestBuffer, &column, &row, &ColumnName);
+	*rowCount = row;
+	*columnCount = column;
+	*columnName = ColumnName;
+
+	return Result;
+}
+
+char*** ShowAllFlights(char*** columnName, int* rowCount, int* columnCount)
+{
+	char requestBuffer[1000] = "SELECT * FROM flights;";
 	char** ColumnName = NULL;
 	int row = 0;
 	int column = 0;
