de050b320fcc86b59cae0bceb089fc1dbfba1603
angelina <angelinadrepakova001@gmail.com>
2020-03-26 13:56:42 +0300

    Add new testHelicopterFly

---
+++ tst/HelicopterFlyTest.cpp
@@ -0,0 +1,94 @@
+#include "gtest/gtest.h"
+extern  "C" {
+#include "../include/BDfunctions.h"
+#include "stdlib.h"
+}
+
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(1, &b, &c, &d);
+	EXPECT_STREQ(result[0][0], "9");
+	EXPECT_STREQ(result[0][1], "1086");
+}
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(1, &b, &c, &d);
+	EXPECT_STRNE(result[0][0], "7");
+	EXPECT_STRNE(result[0][1], "1");
+}
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(2, &b, &c, &d);
+	EXPECT_STREQ(result[0][0], "15");
+	EXPECT_STREQ(result[0][1], "985");
+}
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(2, &b, &c, &d);
+	EXPECT_STRNE(result[0][0], "-1");
+	EXPECT_STRNE(result[0][1], "891");
+}
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(3, &b, &c, &d);
+	EXPECT_STREQ(result[0][0], "4");
+	EXPECT_STREQ(result[0][1], "1006");
+}
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(3, &b, &c, &d);
+	EXPECT_STRNE(result[0][0], "5");
+	EXPECT_STRNE(result[0][1], "1001");
+}
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(4, &b, &c, &d);
+	EXPECT_STREQ(result[0][0], "15");
+	EXPECT_STREQ(result[0][1], "995");
+}
+TEST(test311, timesType) {
+
+	char*** result;
+	char** ID = NULL;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	result = HelicopterFlyDurationAndFlyingResourse(4, &b, &c, &d);
+	EXPECT_STRNE(result[0][0], "5");
+	EXPECT_STRNE(result[0][1], "101");
+}
