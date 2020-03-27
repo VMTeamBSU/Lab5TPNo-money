8942741e128b75c2966950e1e4e9906f912c1745
angelina <angelinadrepakova001@gmail.com>
2020-03-28 00:18:27 +0300

    Update tests

--- tst/ShowAllCommanders.cpp
+++ tst/ShowAllCommanders.cpp
@@ -50,30 +50,30 @@ TEST(testShowAllCommanders, testinginvalidShowAllCommanders) {
 
 
 
-	EXPECT_STREQ(result[0][0], "1");
-	EXPECT_STREQ(result[0][1], "Poletov");
-	EXPECT_STREQ(result[0][2], "200");
-	EXPECT_STREQ(result[0][3], "Lenina 6");
-	EXPECT_STREQ(result[0][4], "1980-10-29");
-	EXPECT_STREQ(result[0][5], " ");
-	EXPECT_STREQ(result[1][0], "0");
-	EXPECT_STREQ(result[1][1], "9of");
-	EXPECT_STREQ(result[1][2], "201");
-	EXPECT_STREQ(result[1][3], "Sadovaya 12");
-	EXPECT_STREQ(result[1][4], "1975-01-22");
-	EXPECT_STREQ(result[1][5], "2");
-	EXPECT_STREQ(result[2][0], "i");
-	EXPECT_STREQ(result[2][1], "Vertoletov");
-	EXPECT_STREQ(result[2][2], "383");
-	EXPECT_STREQ(result[2][3], "Marksa 21");
-	EXPECT_STREQ(result[2][4], "1978-02-14");
-	EXPECT_STREQ(result[2][5], "3");
-	EXPECT_STREQ(result[3][0], "94");
-	EXPECT_STREQ(result[3][1], "Koltun");
-	EXPECT_STREQ(result[3][2], "2785");
-	EXPECT_STREQ(result[3][3], "Jasminova 13");
-	EXPECT_STREQ(result[3][4], "1979-03-15");
-	EXPECT_STREQ(result[3][5], "4");
+	EXPECT_STRNE(result[0][0], "1");
+	EXPECT_STRNE(result[0][1], "Poletov");
+	EXPECT_STRNE(result[0][2], "200");
+	EXPECT_STRNE(result[0][3], "Lenina 6");
+	EXPECT_STRNE(result[0][4], "1980-10-29");
+	EXPECT_STRNE(result[0][5], " ");
+	EXPECT_STRNE(result[1][0], "0");
+	EXPECT_STRNE(result[1][1], "9of");
+	EXPECT_STRNE(result[1][2], "201");
+	EXPECT_STRNE(result[1][3], "Sadovaya 12");
+	EXPECT_STRNE(result[1][4], "1975-01-22");
+	EXPECT_STRNE(result[1][5], "2");
+	EXPECT_STRNE(result[2][0], "i");
+	EXPECT_STRNE(result[2][1], "Vertoletov");
+	EXPECT_STRNE(result[2][2], "383");
+	EXPECT_STRNE(result[2][3], "Marksa 21");
+	EXPECT_STRNE(result[2][4], "1978-02-14");
+	EXPECT_STRNE(result[2][5], "3");
+	EXPECT_STRNE(result[3][0], "94");
+	EXPECT_STRNE(result[3][1], "Koltun");
+	EXPECT_STRNE(result[3][2], "2785");
+	EXPECT_STRNE(result[3][3], "Jasminova 13");
+	EXPECT_STRNE(result[3][4], "1979-03-15");
+	EXPECT_STRNE(result[3][5], "4");
 }
 
 
